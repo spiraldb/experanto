6a1b3fedb70e4c013c98dfe5a75ec03362f07f03
delta003 <markobakovic95@gmail.com>
2026-01-27 17:03:34 +0000

    dl

--- scan.py
+++ scan.py
@@ -1,15 +1,12 @@
 from spiral import Spiral
 import tqdm
 
-sp = Spiral(overrides={
-    "manifests_cache.enabled": "1",
-    "manifests_cache.memory_capacity_bytes": "0",
-    "manifests_cache.disk_capacity_bytes": "1073741824",  # 1GiB
-})
+sp = Spiral()
 project = sp.project("enigma-spiral-poc-2-724186")
 tbl_vjepa_embeddings = project.table("vjepa_embeddings")
 
-vjepa_embeddings_scan = sp.scan(tbl_vjepa_embeddings["tensor"], where=tbl_vjepa_embeddings["layer"] == 4)
+vjepa_embeddings_scan = sp.scan(tbl_vjepa_embeddings["tensor"])
+# vjepa_embeddings_scan = sp.scan(tbl_vjepa_embeddings["tensor"], where=tbl_vjepa_embeddings["layer"] == 4)
 
 print("Scan...\n")
 for batch in tqdm.tqdm(vjepa_embeddings_scan.to_record_batches(hide_progress_bar=True)):
--- scan_example.py
+++ scan_example.py
@@ -1,11 +1,7 @@
 from spiral import Spiral
 import time
 
-sp = Spiral(overrides={
-    # "manifests_cache.enabled": "1",
-    # "manifests_cache.memory_capacity_bytes": "0",
-    # "manifests_cache.disk_capacity_bytes": "1073741824",  # 1GiB
-})
+sp = Spiral(overrides={})
 project = sp.project("public-177331")  # Spiral project
 table = project.table("enigma-embeddings-v3")
 
@@ -19,10 +15,8 @@ for batch in scan.to_record_batches():
     num_rows += batch.num_rows
 et = time.time()
 
-# NOTE(marko): This is an example of suboptimal layout for embeddings.
-#   Needs to scan 9 ~110MBs files, only to get ~135MBs of embeddings.
 print(
-    f"Scanned session/layer 0 in {et - st:.2f} seconds; "
+    f"Scanned in {et - st:.2f} seconds; "
     f"got {num_rows} rows / {float(num_rows * 1408 * 4) / (1024 * 1024)} MBs"
 )
 scan._dump_metrics()
